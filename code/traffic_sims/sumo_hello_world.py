f0793a1bab7a9cbc90e8079d170fc05fe24315e8
Gaetan Marceau <gaetan.marceau.caron@mila.quebec>
2020-01-14 20:24:08 +0000

    Opening the experiment class

--- code/traffic_sims/sumo_hello_world.py
+++ code/traffic_sims/sumo_hello_world.py
@@ -1,3 +1,5 @@
+import logging
+import numpy as np
 from flow.networks.ring import RingNetwork
 
 from flow.core.params import VehicleParams
@@ -14,8 +16,9 @@ from flow.core.params import SumoParams
 from flow.envs.ring.accel import ADDITIONAL_ENV_PARAMS
 from flow.core.params import EnvParams
 
-from flow.core.experiment import Experiment
+# from flow.core.experiment import Experiment
 
+import pdb
 
 vehicles = VehicleParams()
 
@@ -41,7 +44,7 @@ initial_config = InitialConfig(spacing="uniform", perturbation=1)
 
 traffic_lights = TrafficLightParams()
 
-sumo_params = SumoParams(sim_step=0.1, render=True, emission_path='data')
+sumo_params = SumoParams(sim_step=0.1, render=False, emission_path='data')
 
 env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
 
@@ -55,12 +58,66 @@ network = RingNetwork(name="ring_example",
 
 # create the environment object
 env = AccelEnv(env_params, sumo_params, network)
+# num_steps = env.env_params.horizon
+num_steps = 10
+print('num steps {}'.format(num_steps))
+
+NUM_RUNS = 10
+
+
+def rl_actions(*_):
+    return None
+
+
+info_dict = {}
+rets = []
+mean_rets = []
+ret_lists = []
+vels = []
+mean_vels = []
+std_vels = []
+outflows = []
+for i in range(NUM_RUNS):
+    vel = np.zeros(num_steps)
+    logging.info("Iter #" + str(i))
+    ret = 0
+    ret_list = []
+    state = env.reset()
+    for j in range(num_steps):
+        state, reward, done, _ = env.step(rl_actions(state))
+        vel[j] = np.mean(
+            env.k.vehicle.get_speed(env.k.vehicle.get_ids()))
+        ret += reward
+        ret_list.append(reward)
+
+        if done:
+            break
+    rets.append(ret)
+    vels.append(vel)
+    mean_rets.append(np.mean(ret_list))
+    ret_lists.append(ret_list)
+    mean_vels.append(np.mean(vel))
+    std_vels.append(np.std(vel))
+    outflows.append(env.k.vehicle.get_outflow_rate(int(500)))
+    print("Round {0}, return: {1}".format(i, ret))
+
+info_dict["returns"] = rets
+info_dict["velocities"] = vels
+info_dict["mean_returns"] = mean_rets
+info_dict["per_step_returns"] = ret_lists
+info_dict["mean_outflows"] = np.mean(outflows)
+
+print("Average, std return: {}, {}".format(
+    np.mean(rets), np.std(rets)))
+print("Average, std speed: {}, {}".format(
+    np.mean(mean_vels), np.std(mean_vels)))
+env.terminate()
 
 # create the experiment object
-exp = Experiment(env)
+# exp = Experiment(env)
 
 # run the experiment for a set number of rollouts / time steps
-_ = exp.run(1, 3000, convert_to_csv=True)
+# _ = exp.run(1, 3000, convert_to_csv=True)
 
 
-print(ADDITIONAL_ENV_PARAMS)
+# print(ADDITIONAL_ENV_PARAMS)
